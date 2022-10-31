1cf1f8f3986b50e17f1ddeb3bc174e63d1df7a8e
pechenyee <zinakovak67@gmail.com>
2022-10-31 20:47:05 +0300

    Created Mode Controller

---
+++ Assets/Scripts/ActionMode.cs
@@ -0,0 +1,1 @@
+public enum ActionMode { Flag, Shovel };
---
+++ Assets/Scripts/ModeController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class ModeController : MonoBehaviour
+{
+    public Image imageModeController;
+    public Sprite flag;
+    public Sprite shovel;
+
+
+    private ActionMode _currentActionMode = ActionMode.Shovel;
+    public ActionMode currentActionMode => _currentActionMode;
+    
+    public void OnClick()
+    {
+        if (_currentActionMode == ActionMode.Shovel)
+        {
+            _currentActionMode = ActionMode.Flag;
+            imageModeController.sprite = flag;
+        }
+        else
+        {
+            _currentActionMode = ActionMode.Shovel;
+            imageModeController.sprite = shovel;
+        }
+    }
+
+    
+}
